734b3a7e69f355388bde92acecd4c2d9110bcaaf
cd-34 <85720817+cd-34@users.noreply.github.com>
2025-05-21 16:38:10 -0400

    add solutions to 02, 03, 04, and 05

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -9,8 +9,20 @@
 
 // Add your code right below, good luck!
 
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
 
 
+// const greeting = "Hello, my name is " + firstName + " " + lastName + ". I am " + (thisYear-birthYear) + " years old.";
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old."
+//console.log(thisYear-birthYear);
+// console.log(fullName);
+// console.log(age);
+console.log(greeting);
 
 
 // Do not change this
